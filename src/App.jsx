6de5ceb1113b6d9b673896ffa85421b045a284d7
mohs-in <in.mohsin@outlook.com>
2024-04-16 23:10:17 +0530

    added header description

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 
-import './App.css'
+import './App.scss'
 
 function App() {
 
   return (
     <>
-      <h1 className='raleway-font'>Mohsin Khansab</h1>
+    <header>
+
+      <div className='header--name'>
+
+        <h1>Mohsin <br /> Khansab</h1>
+      
+      </div>
+
+      <div className='header--desc'>
+
+        <p>Full-stack Software Engineer. <br />Currently improving my proficiency in writing Clean, Efficient and Scalable code. <br />
+        Tech I'm interested in : <br /> <span>React, Typescript, Node.js, Next.js, Express.js</span></p>
+
+      </div>
+      
+    </header>
+      
     </>
   )
 }
