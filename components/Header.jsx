bba0562554cf32c775377153b31a12f79bf2c9b1
mohs-in <in.mohsin@outlook.com>
2024-04-17 00:36:38 +0530

    new header component added

---
+++ components/Header.jsx
@@ -0,0 +1,21 @@
+export default function Header() {
+    return (
+        <header>
+
+            <div className='header--name'>
+
+                <h1>Mohsin <br /> Khansab</h1>
+            
+            </div>
+
+            <div className='header--desc'>
+
+                <p>Full-stack Software Engineer. <br />Currently improving my proficiency in writing Clean, Efficient and Scalable code. <br />
+
+                <span className='skills'>Tech Stack I'm interested in : <br /> React, Typescript, Next.js, Express, Node.js</span></p>
+
+            </div>
+
+        </header>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-
+import Header from '../components/Header'
 import './App.scss'
 
 function App() {
 
   return (
     <>
-    <header>
-
-      <div className='header--name'>
-
-        <h1>Mohsin <br /> Khansab</h1>
-      
-      </div>
-
-      <div className='header--desc'>
-
-        <p>Full-stack Software Engineer. <br />Currently improving my proficiency in writing Clean, Efficient and Scalable code. <br />
-        Tech I'm interested in : <br /> <span>React, Typescript, Node.js, Next.js, Express.js</span></p>
-
-      </div>
-      
-    </header>
-      
+      <Header />  
     </>
   )
 }
