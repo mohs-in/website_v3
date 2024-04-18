99f57be4fca55a39ff03ab0a1cee18ea5f672acc
mohs-in <in.mohsin@outlook.com>
2024-04-18 21:02:00 +0530

    update social links

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,6 @@
+import gh from '/gh-2d.svg'
+import lin from '/insvg.svg'
+
 export default function Header() {
     return (
         <header>
@@ -13,7 +16,12 @@ export default function Header() {
                 <p>Full-stack Software Engineer. <br />Currently improving my proficiency in writing Clean, Efficient and Scalable code. <br />
 
                 <span className='skills'>Tech Stack I'm interested in : <br /> React, Typescript, Next.js, Express, Node.js</span></p>
-
+                
+                <div className='social-links'>
+                    <a href="https://github.com/mohs-in" target='_blank'><img src={gh} alt="" /></a>
+                    <a href="https://www.linkedin.com/in/mohsinkhansab" target='_blank'><img src={lin} alt="" /></a>
+                </div>
+                
             </div>
 
         </header>
