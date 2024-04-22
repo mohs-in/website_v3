f594727bc4f23569d6add4da49dfb0faaa9c9877
mohs-in <in.mohsin@outlook.com>
2024-04-23 00:39:51 +0530

    update projects data

--- components/projects.js
+++ components/projects.js
@@ -1,17 +1,58 @@
 
 export default [
     {
-        date: '01-09-2078',
-        title: 'headingOne',
-        description: 'describe',
-        github: 'https://www.google.com',
-        demo: 'https://www.example.com'
+        date: '04-19-2024',
+        title: 'Leads Tracker',
+        description: "A chrome extension to save URL's",
+        github: 'https://github.com/mohs-in/leads-tracker',
     },
     {
-        date: '01-09-2078',
-        title: 'headingTwo',
-        description: 'describe',
-        github: 'https://www.google.com',
-        demo: 'https://www.example.com'
+        date: '04-03-2024',
+        title: 'Tenzies Game',
+        description: 'A fun game to play built w/ React.js',
+        github: 'https://github.com/mohs-in/tenzies-game',
+        demo: 'https://tenzies.mohs.ink/'
+    },
+    {
+        date: '03-17-2024',
+        title: 'Meme Generator',
+        description: 'Generate memes with custom text',
+        github: 'https://github.com/mohs-in/meme-generator',
+        demo: 'https://generatememe.mohs.ink'
+    },
+    {
+        date: '03-12-2024',
+        title: 'GPT',
+        description: 'Get Passwords in no Time',
+        github: 'https://github.com/mohs-in/password-generator',
+        demo: 'https://generatepassword.mohs.ink/'
+    },
+    {
+        date: '03-08-2024',
+        title: 'NFT Site',
+        description: 'Built with React.js & Sass',
+        github: 'https://github.com/mohs-in/nft-site',
+        demo: 'https://nft.mohs.ink'
+    },
+    {
+        date: '03-05-2024',
+        title: 'WeBarelyWork',
+        description: 'Explore workspaces for lazy devs',
+        github: 'https://github.com/mohs-in/cowork-space',
+        demo: 'https://workspace.mohs.ink'
+    },
+    {
+        date: '02-24-2024',
+        title: 'Travel Journal',
+        description: 'Places I have been to in imagination',
+        github: 'https://github.com/mohs-in/travel-journal',
+        demo: 'https://traveljournal.mohs.ink'
+    },
+    {
+        date: '02-20-2024',
+        title: 'Airbnb Site',
+        description: 'Airbnb Clone Basic',
+        github: 'https://github.com/mohs-in/airbnb-clone',
+        demo: 'https://airbnbclone.mohs.ink'
     }
 ]
