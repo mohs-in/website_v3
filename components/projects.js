f71f3f81255513222a9d7190217de0363424de92
mohs-in <in.mohsin@outlook.com>
2024-06-08 21:29:59 +0530

    add van-life project

--- components/projects.js
+++ components/projects.js
@@ -1,30 +1,38 @@
 
 export default [
     {
-        id:1,
+        id:9,
+        date: 'Ongoing...',
+        title: 'Van Life',
+        description: "App made using React router",
+        github: 'https://github.com/mohs-in/van-life',
+        demo: 'https://vanlife.mohs.ink/'
+    },
+    {
+        id:8,
         date: '19 Apr 2024',
         title: 'Leads Tracker',
-        description: "A chrome extension to save URL's",
+        description: "Extension to save URL's",
         github: 'https://github.com/mohs-in/leads-tracker',
     },
     {
-        id:2,
+        id:7,
         date: '3 Apr 2024',
         title: 'Tenzies Game',
-        description: 'A fun game to play built w/ React.js',
+        description: 'A fun game to play in free time',
         github: 'https://github.com/mohs-in/tenzies-game',
         demo: 'https://tenzies.mohs.ink/'
     },
     {
-        id:3,
+        id:6,
         date: '17 Mar 2024',
         title: 'Meme Generator',
-        description: 'Generate memes with custom text',
+        description: 'Memes w/ custom text',
         github: 'https://github.com/mohs-in/meme-generator',
         demo: 'https://generatememe.mohs.ink'
     },
     {
-        id:4,
+        id:5,
         date: '12 Mar 2024',
         title: 'GPT',
         description: 'Get Passwords in no Time',
@@ -32,7 +40,7 @@ export default [
         demo: 'https://generatepassword.mohs.ink/'
     },
     {
-        id:5,
+        id:4,
         date: '8 Mar 2024',
         title: 'NFT Site',
         description: 'Built with React.js & Sass',
@@ -40,23 +48,23 @@ export default [
         demo: 'https://nft.mohs.ink'
     },
     {
-        id:6,
+        id:3,
         date: '5 Mar 2024',
         title: 'WeBarelyWork',
-        description: 'Explore workspaces for lazy devs',
+        description: 'Workspaces for lazy devs',
         github: 'https://github.com/mohs-in/cowork-space',
         demo: 'https://workspace.mohs.ink'
     },
     {
-        id:7,
+        id:2,
         date: '24 Feb 2024',
         title: 'Travel Journal',
-        description: 'Places I have been to in imagination',
+        description: 'Places for exploration',
         github: 'https://github.com/mohs-in/travel-journal',
         demo: 'https://traveljournal.mohs.ink'
     },
     {
-        id:8,
+        id:1,
         date: '20 Feb 2024',
         title: 'Airbnb Site',
         description: 'Airbnb Clone Basic',
