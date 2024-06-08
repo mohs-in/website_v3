a717d9c6f9c601abe80001fe64b41cf45917676a
mohs-in <in.mohsin@outlook.com>
2024-06-09 03:07:54 +0530

    update custom tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,49 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        myFont: ['myFont', 'sans-serif'],
+        myFontOne: ['myFontOne', 'sans-serif']
+      },
+      colors: {
+        light: {
+          primary: '#f1f1f1',
+          secondary: '#d6d6d6',
+        },
+        dark: {
+          primary: '#101010',
+          secondary: '#1a1a1a',
+        },
+        grey: '#343434',
+        borderColor: '#e5e7eb',
+      },
+      maxWidth: {
+        '24em': '24em',
+        '14em': '14em',
+      },
+      spacing: {
+        '55rem': '55rem',
+      },
+      borderRadius: {
+        '1.25': '1.25rem',
+      },
+      backgroundColor: {
+        'bgGrey': '#70707008'
+      },
+      width: {
+        '72px': '72px',
+        '22px': '22px'
+      },
+      height: {
+        '22px': '22px'
+      },
+      animation: {
+        'spin-slow': 'spin 3s',
+        'bounce-2': 'bounce 3s infinite'
+      }
+    },
   },
   plugins: [],
 }
+
