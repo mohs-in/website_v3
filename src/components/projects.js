043342732fad55d3b6b37fe7b7fc5a6e84680c1c
mohs-in <in.mohsin@outlook.com>
2024-06-24 18:32:10 +0530

    update projects data

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,74 +1,66 @@
 
 export default [
     {
-        id:9,
+        id:8,
         date: 'Ongoing...',
         title: 'Van Life',
         description: "App made using React router",
         github: 'https://github.com/mohs-in/van-life',
-        demo: 'https://vanlife.mohs.ink/'
+        demo: 'https://vanlife-ms.vercel.app/'
     },
     {
-        id:8,
+        id:7,
         date: '19 Apr 2024',
         title: 'Leads Tracker',
         description: "A chrome extension to save URL's",
         github: 'https://github.com/mohs-in/leads-tracker',
     },
     {
-        id:7,
+        id:6,
         date: '3 Apr 2024',
         title: 'Tenzies Game',
         description: 'A fun game to play built w/ React.js',
         github: 'https://github.com/mohs-in/tenzies-game',
-        demo: 'https://tenzies.mohs.ink/'
+        demo: 'https://tenzies-ms.vercel.app/'
     },
     {
-        id:6,
+        id:5,
         date: '17 Mar 2024',
         title: 'Meme Generator',
         description: 'Generate memes with custom text',
         github: 'https://github.com/mohs-in/meme-generator',
-        demo: 'https://generatememe.mohs.ink'
+        demo: 'https://memegen-ms.vercel.app/'
     },
     {
-        id:5,
+        id:4,
         date: '12 Mar 2024',
         title: 'GPT',
         description: 'Get Passwords in no Time',
         github: 'https://github.com/mohs-in/password-generator',
-        demo: 'https://generatepassword.mohs.ink/'
+        demo: 'https://passgen-ms.vercel.app/'
     },
     {
-        id:4,
+        id:3,
         date: '8 Mar 2024',
         title: 'NFT Site',
         description: 'Built with React.js & Sass',
         github: 'https://github.com/mohs-in/nft-site',
-        demo: 'https://nft.mohs.ink'
+        demo: 'https://nft-ms.vercel.app/'
     },
     {
-        id:3,
+        id:2,
         date: '5 Mar 2024',
         title: 'WeBarelyWork',
         description: 'Explore workspaces for lazy devs',
         github: 'https://github.com/mohs-in/cowork-space',
-        demo: 'https://workspace.mohs.ink'
+        demo: 'https://workspace-ms.vercel.app/'
     },
     {
-        id:2,
+        id:1,
         date: '24 Feb 2024',
         title: 'Travel Journal',
         description: 'Places for exploration',
         github: 'https://github.com/mohs-in/travel-journal',
-        demo: 'https://traveljournal.mohs.ink'
-    },
-    {
-        id:1,
-        date: '20 Feb 2024',
-        title: 'Airbnb Site',
-        description: 'Airbnb Clone Basic',
-        github: 'https://github.com/mohs-in/airbnb-clone',
-        demo: 'https://airbnbclone.mohs.ink'
+        demo: 'https://traveljournal-ms.vercel.app/'
     }
 ]
