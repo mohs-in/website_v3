35fa2a7413298fd66c3b152ae2c6510842111339
Mohsin <in.mohsin@outlook.com>
2024-11-02 00:58:43 +0530

    Added time display

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,14 @@
 import Header from './Header'
 import Body from './Body'
+import Time from './Time'
 
 export default function Hero() {
+
     return (
         <>
             <div className='flex items-center justify-center'>
-                <div className='lg:max-w-4xl'>
+                <div className='lg:max-w-4xl'>  
+                    <Time />            
                     <Header />
                     <Body />
                 </div>
@@ -13,3 +16,4 @@ export default function Hero() {
         </>
     )
 }
+
---
+++ src/components/Time.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import format from 'date-fns/format';
+
+
+export default function Time(){
+    console.log(`time comp re-rendered`);
+    const time = useTime();
+
+    return (
+        <>
+            <p className="absolute top-[1rem] right-[.5rem]">
+                {format(time, 'hh:mm:ss a')}
+            </p>  
+        </>
+    )
+}
+function useTime() {
+    const [time, setTime] = React.useState(new Date());
+
+    React.useEffect(() => {
+        const intervalId = window.setInterval(() => {
+            setTime(new Date());
+        }, 1000)
+
+        return () => {
+            window.clearInterval(intervalId);
+        }
+    }, [])
+
+    return time;
+}
