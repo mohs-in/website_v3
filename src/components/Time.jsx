142150e95901e57c21e8b8e60d91dac7ea921617
Mohsin <in.mohsin@outlook.com>
2024-11-02 22:33:26 +0530

    Removed unused code

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -3,27 +3,26 @@ import format from 'date-fns/format';
 
 
 export default function Time(){
-    console.log(`time comp re-rendered`);
     const time = useTime();
 
     return (
         <>
-            <p className="absolute top-[1rem] right-[.5rem]">
-                {format(time, 'hh:mm:ss a')}
+            <p className = " absolute top-[1rem] right-[.5rem] ">
+                { format( time, 'hh:mm:ss a' ) }
             </p>  
         </>
     )
 }
 function useTime() {
-    const [time, setTime] = React.useState(new Date());
+    const [ time, setTime ] = React.useState( new Date() );
 
-    React.useEffect(() => {
-        const intervalId = window.setInterval(() => {
-            setTime(new Date());
+    React.useEffect( () => {
+        const intervalId = window.setInterval( () => {
+            setTime( new Date() );
         }, 1000)
 
         return () => {
-            window.clearInterval(intervalId);
+            window.clearInterval( intervalId );
         }
     }, [])
 
