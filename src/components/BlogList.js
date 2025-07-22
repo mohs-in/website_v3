80960a5e806cc62190464da78f2c1aea0714b8f6
mohs-in <in.mohsin@outlook.com>
2025-07-22 22:14:35 +0530

    added new blog

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,5 +1,11 @@
 export default [
     {
+        id: 3,
+        title: "Selection Sort",
+        description: "Selection Sort Algorithm explained",
+        link: "https://mohsinkhansab.hashnode.dev/selection-sort-algorithm-in-javascript"
+    },
+    {
         id: 2,
         title: "Cloud Computing",
         description: "Getting Started w/ cloud computing, check out this article",
